83d2cf8c3e730aa4bff150e35b9883685d49f945
Chip Bell <chip.bell@spinen.com>
2015-01-14 00:01:14 -0500

    Finishing up deploy command

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,9 @@
 
 set -e
 
-tar czvf bundle.tar.gz *
-scp bundle.tar.gz aprilandchip@aprilandchip.com:~/html/math6730
+echo "Bundling"
+tar czf bundle.tar.gz *
+echo "Copying file"
+scp bundle.tar.gz aprilandchip@aprilandchip.com:~/html/cse6730/bundle.tar.gz
+echo "Unbundling"
+ssh aprilandchip@aprilandchip.com "cd html/cse6730/ && tar xzf bundle.tar.gz"
