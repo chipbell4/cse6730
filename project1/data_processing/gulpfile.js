fcca41cf6b51be81f265ad787af9ff3b64f11ed4
Chip Bell <chip.bell@spinen.com>
2015-01-17 11:51:48 -0500

    Playing with input streams

--- project1/data_processing/gulpfile.js
+++ project1/data_processing/gulpfile.js
@@ -1,4 +1,16 @@
 var gulp = require('gulp');
+var csv = require('csv');
+var fs = require('fs');
 
-gulp.task('default', function() {
+gulp.task('input-distribution', function(taskDone) {
+    fs.createReadStream('demo.csv')
+        .pipe(csv.parse())
+        .pipe(csv.transform(function(row, rowFinished) {
+            // check if 
+        }))
+        .pipe(csv.stringify())
+        .pipe(process.stdout);
+});
+
+gulp.task('default', ['input-distribution'], function() {
 });
