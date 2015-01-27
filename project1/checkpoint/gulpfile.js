fa7d1882fa873a63283d6dd54dbe30cbcaafd62a
Chip Bell <chip.bell@spinen.com>
2015-01-27 18:11:50 -0500

    Getting latex build to work

--- project1/checkpoint/gulpfile.js
+++ project1/checkpoint/gulpfile.js
@@ -1,4 +1,30 @@
 var gulp = require('gulp');
+var async = require('async');
+var exec = require('child_process').exec;
+var del = require('del');
 
-gulp.task('default', function() {
+// Converts a string command into an async exec call, to be chained with async.series
+var asyncCommand = function(commandText) {
+    return function(callback) {
+        exec(commandText, callback);
+    };
+};
+
+gulp.task('clean', function(cb) {
+    del(['*.{aux,bbl,blg,dvi,log,pdf}'], cb);
+});
+
+
+gulp.task('default', ['clean'], function(cb) {
+    var latex = 'latex -halt-on-error main';
+    var bibtex = 'bibtex main';
+    var dvipdf = 'dvipdf main.dvi main.pdf';
+    
+    async.series([
+        asyncCommand(latex),
+        asyncCommand(bibtex),
+        asyncCommand(latex),
+        asyncCommand(latex),
+        asyncCommand(dvipdf)
+    ], cb);
 });
