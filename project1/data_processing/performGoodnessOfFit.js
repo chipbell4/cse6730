850d2faf011d717b65558f35023eb77c63f3ddc5
Chip Bell <chip.bell@spinen.com>
2015-01-28 23:56:35 -0500

    Fixing goodness of fit to use frequencies instead

--- project1/data_processing/performGoodnessOfFit.js
+++ project1/data_processing/performGoodnessOfFit.js
@@ -9,13 +9,16 @@ module.exports = function(rawData, binCount) {
     // The maximum likelihood lambda value 1 / mean
     var mleLambda = 1 / mean(rawData);
 
+    var N = rawData.length;
+
     // the expected frequencies
-    var expectations = expectedExponentialFrequencies(binnedData.binWidth, binCount, mleLambda);
+    var expectations = expectedExponentialFrequencies(binnedData.binWidth, binCount, mleLambda).map(function(percent) {
+        return percent * N;
+    });
 
     // the actual frequencies
-    var N = rawData.length;
     var actualFrequencies = binnedData.bins.map(function(bin) {
-        return bin.length / N;
+        return bin.length;
     });
 
     return chiSquaredTest(actualFrequencies, expectations, 2);
