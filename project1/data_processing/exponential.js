7080355588daf4c3e0aad28facaaa718ea2fe835
Chip Bell <chip.bell@spinen.com>
2015-01-28 21:17:23 -0500

    Adding a helper module for the exponential distribution

---
+++ project1/data_processing/exponential.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+    pdf: function(x, lambda) {
+        return lambda * Math.exp(-lambda * x);
+    },
+    cdf: function(x, lambda) {
+        return 1 - Math.exp(-lambda * x);
+    }
+};
