901092c757c2e31d8b4c258866f6cb7fc24d7fe4
Chip Bell <chip.bell@spinen.com>
2015-02-01 17:02:01 -0500

    Back to basics

--- project1/data_processing/gulpfile.js
+++ project1/data_processing/gulpfile.js
@@ -1,21 +1,5 @@
 var gulp = require('gulp');
-var ngsim = require('./ngsimFilters.js');
-var inputDistribution = require('./inputDistributionTaskFactory.js');
 
-gulp.task('northboundPlatoonLeaders', inputDistribution({
-    direction: ngsim.direction.NORTH,
-    intersection: '3',
-    isLeader: true
-}));
-
-gulp.task('northbound', inputDistribution({
-    direction: ngsim.direction.NORTH,
-    intersection: '3'
-}));
-
-gulp.task('southbound', inputDistribution({
-    direction: ngsim.direction.SOUTH,
-    intersection: '3'
-}));
-
-gulp.task('default', ['northboundPlatoonLeaders', 'northbound']);
+gulp.task('default', function(cb) {
+    cb();
+});
--- project1/data_processing/mean.js
+++
@@ -1,8 +0,0 @@
-/**
- * Calculates the average of a list.
- */
-module.exports = function(items) {
-    return items.reduce(function(accumulator, item) {
-        return accumulator + item;
-    }, 0) / items.length;
-};
