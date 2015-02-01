cf1857fb71d3b19a2565f4dabf83f0b94a4bafea
Chip Bell <chip.bell@spinen.com>
2015-01-31 21:33:00 -0500

    Making task generic

--- project1/data_processing/gulpfile.js
+++ project1/data_processing/gulpfile.js
@@ -1,47 +1,10 @@
 var gulp = require('gulp');
-var csvFilter = require('./csvFilter.js');
 var ngsimFilters = require('./ngsimFilters.js');
-var array_interval = require('./array_interval.js');
-var mean = require('./mean.js');
-var performGoodnessOfFit = require('./performGoodnessOfFit.js');
-var histogram = require('./histogram.js');
+var inputDistributionTaskFactory = require('./inputDistributionTaskFactory.js');
 
-gulp.task('northbound-input-distribution', function(taskDone) {
-
-    var startTimes = [];
-
-    var filters = [];
-
-    // Only northbound
-    filters.push(ngsimFilters.direction(ngsimFilters.direction.NORTH));
-
-    // only through our intersection
-    filters.push(ngsimFilters.intersection('3'));
-
-    // only keep the first occurrences
-    filters.push(ngsimFilters.firstKeeper());
-
-    // track the start times
-    filters.push(function(row) {
-        startTimes.push(Number(row[3]));
-        return row;
-    });
-
-    var finish = function() {
-        // calculate the intervals between arrivals (in seconds)
-        var startOffsets = array_interval(startTimes).map(function(interval) {
-            return interval / 1000;
-        });
-
-        var chiSquaredResults = performGoodnessOfFit(startOffsets, 25);
-        console.log('N = ' + startOffsets.length);
-        console.log("Chi-Squared Test : p = " + chiSquaredResults.probability + " chi^2 = " + chiSquaredResults.chiSquared);
-        console.log(histogram(startOffsets, { }));
-
-        taskDone();
-    };
-
-    csvFilter(filters, finish);
-});
+gulp.task(
+    'northbound-input-distribution',
+    inputDistributionTaskFactory(ngsimFilters.direction.NORTH, '3')
+);
 
 gulp.task('default', ['northbound-input-distribution']);
---
+++ project1/data_processing/inputDistributionTaskFactory.js
@@ -0,0 +1,48 @@
+var csvFilter = require('./csvFilter.js');
+var ngsimFilters = require('./ngsimFilters.js');
+var array_interval = require('./array_interval.js');
+var mean = require('./mean.js');
+var performGoodnessOfFit = require('./performGoodnessOfFit.js');
+var histogram = require('./histogram.js');
+
+/**
+ * Factory function for building a gulp task for filtering the NGSIM data for a certain direction and section of the road
+ */
+module.exports = function(direction, section) {
+    return function(taskDone) {
+        var startTimes = [];
+
+        var filters = [];
+
+        // Only northbound
+        filters.push(ngsimFilters.direction(direction));
+
+        // only through our intersection
+        filters.push(ngsimFilters.intersection(section));
+
+        // only keep the first occurrences
+        filters.push(ngsimFilters.firstKeeper());
+
+        // track the start times
+        filters.push(function(row) {
+            startTimes.push(Number(row[3]));
+            return row;
+        });
+
+        var finish = function() {
+            // calculate the intervals between arrivals (in seconds)
+            var startOffsets = array_interval(startTimes).map(function(interval) {
+                return interval / 1000;
+            });
+
+            var chiSquaredResults = performGoodnessOfFit(startOffsets, 25);
+            console.log('N = ' + startOffsets.length);
+            console.log("Chi-Squared Test : p = " + chiSquaredResults.probability + " chi^2 = " + chiSquaredResults.chiSquared);
+            console.log(histogram(startOffsets, { }));
+
+            taskDone();
+        };
+
+        csvFilter(filters, finish);
+    };
+};
