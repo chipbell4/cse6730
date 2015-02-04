fac1ab6471c92342295590c2cf5f3312aeb0d5af
Chip Bell <chip.bell@spinen.com>
2015-02-03 19:12:31 -0500

    Adding a clean task

--- project1/simulation/gulpfile.js
+++ project1/simulation/gulpfile.js
@@ -1,10 +1,15 @@
 var gulp = require('gulp');
+var del = require('del');
 var coffeeify = require('gulp-coffeeify');
 
 gulp.task('bundle', function() {
     return gulp.src('coffee/main.coffee')
         .pipe(coffeeify())
         .pipe(gulp.dest('.'));
+});
+
+gulp.task('clean', function(cb) {
+    del('main.js', cb);
 });
 
 gulp.task('default', ['bundle']);
