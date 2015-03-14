b0a2de75e33c9dc7e7c73aa444637af64d2237fc
Chip Bell <chip.bell@spinen.com>
2015-03-13 20:27:48 -0400

    Adding nice args and stuff

--- project2/wmata/pingApi.sh
+++ project2/wmata/pingApi.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-output_dir="$1"
+# cron schedule (all the time): * * * * * *
 
-if [ -z "$output_dir" ]; then
-    output_dir="."
-fi
+output_dir="."
+subscription_key="NOPE"
+
+while getopts "h?o:?k:" opt; do
+    case "$opt" in
+    h|\?)
+        echo "Usage: ./pingApi.sh -k <API_KEY> [-o output_directory]"
+        exit 0
+        ;;
+    o)
+        output_dir=$OPTARG
+        ;;
+    k)
+        subscription_key="$OPTARG"
+        ;;
+    esac
+done
 
 # format the current date+time
 output_file=`date +"%s.json"`
-
-# pluck my subscription key, stripping newlines and things
-subscription_key=`cat WMATA.txt`
 
 # Hit the api, using my subscription key, and get ALL stations.
 curl -X GET "https://api.wmata.com/StationPrediction.svc/json/GetPrediction/All?api_key=$subscription_key" > $output_dir/$output_file
