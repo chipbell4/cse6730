102eab2e8a49eea5d17a7a39d17837780617fa35
Chip Bell <chip.bell@spinen.com>
2015-03-13 22:13:00 -0400

    Using the helper script to do the appending

--- project2/wmata/insertIntoMongo.sh
+++ project2/wmata/insertIntoMongo.sh
@@ -9,10 +9,10 @@ fi
 
 # convert the json by appending the timestamp of the file on first
 timestamp=`echo $fileToInsert | cut -d'.' -f1`
-cat $fileToInsert | sed "s/^{/{\"timestamp\":$timestamp,/" > document.json
+cat $fileToInsert | python appendTimestamp.py $timestamp > documents.json
 
 # now append into mongo
-mongoimport --db metro --collection trains < document.json
+mongoimport --db metro --collection trains < documents.json
 
 # cleanup
 rm document.json
