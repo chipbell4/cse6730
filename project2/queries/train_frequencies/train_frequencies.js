1d2d51d838b4858dcf2235360996b29eff2d06f1
Chip Bell <chip.bell@spinen.com>
2015-03-25 19:45:46 -0400

    Making query only take weekdays

--- project2/queries/train_frequencies/train_frequencies.js
+++ project2/queries/train_frequencies/train_frequencies.js
@@ -2,7 +2,8 @@ var query = {
     Min: '1',
     Line: '${Line}',
     LocationCode: '${LocationCode}',
-    DestinationCode: '${DestinationCode}'
+    DestinationCode: '${DestinationCode}',
+    timestamp: { $gt: 1426496399 } // After monday
 };
 db.trains.find(query, { timestamp: true }).sort({ timestamp: 1 }).forEach(function(train) {
     print(train.timestamp);
