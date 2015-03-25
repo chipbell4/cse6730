2a325b3031216390301f2891b2094bb011c9d72b
Chip Bell <chip.bell@spinen.com>
2015-03-24 21:32:55 -0400

    Allowing numbers in variables

--- project2/queries/template.sh
+++ project2/queries/template.sh
@@ -8,7 +8,7 @@ template () {
   # first, we convert the properties into a list of SED replacements. This regex is awful, but essentially does this:
   # A line like name=chip becomes a sed command s/${name}/chip/g for replacing "variables" with a particular value
   touch sed_commands.txt
-  cat $2 | sed -E 's/([a-zA-Z]+)=([a-zA-Z]+)/s\/\${\1}\/\2\/g/g' > sed_commands.txt
+  cat $2 | sed -E 's/([a-zA-Z0-9]+)=([a-zA-Z0-9]+)/s\/\${\1}\/\2\/g/g' > sed_commands.txt
 
   # Now we run the sed file on our template file
   local template_results=`cat $1 | sed -f sed_commands.txt`
