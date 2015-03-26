a09fc31f95d308830a4375a2504965529dcb0832
Chip Bell <chip.bell@spinen.com>
2015-03-25 20:02:43 -0400

    Adding minute into query

--- project2/queries/train_frequencies/train_frequencies.js
+++ project2/queries/train_frequencies/train_frequencies.js
@@ -1,10 +1,9 @@
 var query = {
-    Min: '1',
     Line: '${Line}',
     LocationCode: '${LocationCode}',
     DestinationCode: '${DestinationCode}',
     timestamp: { $gt: 1426496399 } // After monday
 };
-db.trains.find(query, { timestamp: true }).sort({ timestamp: 1 }).forEach(function(train) {
-    print(train.timestamp);
+db.trains.find(query, { timestamp: true, Min: true }).forEach(function(train) {
+    print(train.timestamp + ',' + train.Min);
 });
