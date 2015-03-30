92094ee0979d3014e3e540bd5515e68d4a459ca8
Chip Bell <chip.bell@spinen.com>
2015-03-30 16:35:27 -0400

    Stubbing out some latex stuff I'll need

---
+++ project2/paper/gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var async = require('async');
+var exec = require('child_process').exec;
+var del = require('del');
+
+// Converts a string command into an async exec call, to be chained with async.series
+var asyncCommand = function(commandText) {
+    return function(callback) {
+        exec(commandText, callback);
+    };
+};
+
+gulp.task('clean', function(cb) {
+    del(['*.{aux,bbl,blg,dvi,log,pdf}'], cb);
+});
+
+
+gulp.task('default', ['clean'], function(cb) {
+    var latex = 'pdflatex -halt-on-error main';
+    var bibtex = 'bibtex main';
+    
+    async.series([
+        asyncCommand(latex),
+        asyncCommand(bibtex),
+        asyncCommand(latex),
+        asyncCommand(latex)
+    ], cb);
+});
