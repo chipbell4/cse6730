0b9357688b8c93c6b426b16fc120ff64e2759428
Chip Bell <chip.bell@spinen.com>
2015-03-31 10:47:55 -0400

    No uglify right now

--- project2/sim/gulpfile.js
+++ project2/sim/gulpfile.js
@@ -6,7 +6,7 @@ var uglify = require('gulp-uglify');
 gulp.task('bundle', function() {
     return gulp.src('coffee/main.coffee')
         .pipe(coffeeify())
-        .pipe(uglify({ "source-map" : "main.js.map" }))
+        //.pipe(uglify({ "source-map" : "main.js.map" }))
         .pipe(gulp.dest('.'));
 });
 
