0d3ced6783e0af12f50d19232dc935cc54bbd76c
Chip Bell <chip.bell@spinen.com>
2015-04-07 19:36:19 -0400

    Getting testing working

--- project2/sim/gulpfile.js
+++ project2/sim/gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var del = require('del');
 var coffeeify = require('gulp-coffeeify');
+var mocha = require('gulp-mocha');
 var uglify = require('gulp-uglify');
 
 gulp.task('bundle', function() {
@@ -8,6 +9,17 @@ gulp.task('bundle', function() {
         .pipe(coffeeify())
         //.pipe(uglify({ "source-map" : "main.js.map" }))
         .pipe(gulp.dest('.'));
+});
+
+gulp.task('test', function() {
+    // So the coffee transform for mocha works
+    require('coffee-script/register');
+
+    return gulp.src('test/**/*.coffee')
+        .pipe(mocha({
+            reporter: 'spec',
+            compilers: 'coffee:coffee-script'
+        }));
 });
 
 gulp.task('clean', function(cb) {
---
+++ project2/sim/test/dummy.coffee
@@ -0,0 +1,5 @@
+expect = require('chai').expect
+
+describe 'Dummy Test', ->
+    it 'Should totally work', ->
+        expect(1).to.equal(1)
