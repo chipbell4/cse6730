44a9ca71178c5e626b00b645122d34f53f9e8a07
Chip Bell <chip.bell@spinen.com>
2015-04-19 17:02:31 -0400

    Minifying again now

--- project2/sim/gulpfile.js
+++ project2/sim/gulpfile.js
@@ -7,7 +7,7 @@ var uglify = require('gulp-uglify');
 gulp.task('bundle', function() {
     return gulp.src('coffee/main.coffee')
         .pipe(coffeeify())
-        //.pipe(uglify({ "source-map" : "main.js.map" }))
+        .pipe(uglify({ "source-map" : "main.js.map" }))
         .pipe(gulp.dest('.'));
 });
 
